696528ca3a0120b4e3cfb1f7783ecd404891c28f
Rdohrman <39611193+Rdohrman@users.noreply.github.com>
2020-08-21 01:11:47 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,47 +1,24 @@
 <?php
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-  $Fname = $_POST['Fname'];
-  $Lname = $_POST['Lname'];
-  $email = $_POST['email'];
-
-  //Database connection
-
-  if (!empty($username) || !empty($password) || !empty($Fname) || !empty($Lname) || !empty($email)){
-    $host = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $dbname = "curatelogin";
-
-    $conn = new mysqli($host,$dbUsername,$dbPassword, $dbname);
-
-    if (mysqli_connect_error()) {
-      die ('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
-    } else {
-      $SELECT = "SELECT email From register Where email = ? Limit 1";
-      $INSERT = "INSERT Into register (username, password, Fname, Lname, email) values (?, ?, ?, ?, ?)";
-
-      $stmt = $conn->prepare($SELECT);
-      $stmt->bind_param("s", $email);
-      $stmt->execute();
-      $stmt->bind_result($email);
-      $stmt->store_result();
-      $rnum = $stmt->num_rows;
-      if ($rnum == 0) {
-        $stmt->close();
-
-        $stmt = $conn->prepare($INSERT);
-        $stmt->bind_param("sssss", $username, $password, $Fname, $Lname, $email);
-        $stmt->execute()
-        echo "New record inserted successfully.";
-        {else}
-        echo "Someone has already registered with this email.";
-      }
-      $stmt->close();
-      $conn->close();
-    }
-  } else {
-    echo "All fields are required.";
-    die();
-  }
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "users";
+ 
+// Create connection
+$conn = new mysqli($curatelogin, $root, $, $users);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+ 
+$sql = "INSERT INTO users (username, password, FName, LName, email)
+VALUES ('whatup', 'test', 'John', 'Doe', 'john@example.com')";
+ 
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+ 
+$conn->close();
 ?>
